2d3ceb6fa48527bf4aa77d83e4690a4fe251b3ec
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-04-17 10:14:11 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  vue: true,
+  formatters: {
+    css: true,
+  },
+  rules: {
+    'no-console': 'off',
+
+    'array-bracket-newline': ['error', 'consistent'],
+    'array-element-newline': ['error', 'consistent'],
+
+    // stylistic
+    'ts/adjacent-overload-signatures': 'error',
+    'ts/array-type': 'error',
+    'ts/ban-tslint-comment': 'error',
+    'ts/class-literal-property-style': 'error',
+    'ts/consistent-generic-constructors': 'error',
+    'ts/consistent-indexed-object-style': 'error',
+    'ts/consistent-type-definitions': 'error',
+    'ts/no-confusing-non-null-assertion': 'error',
+    'no-empty-function': 'off',
+    'ts/no-empty-function': 'error',
+    'ts/no-empty-interface': 'error',
+    'ts/no-inferrable-types': 'error',
+    'ts/prefer-for-of': 'error',
+    'ts/prefer-function-type': 'error',
+    'ts/prefer-namespace-keyword': 'error',
+  },
+})
---
+++ src/dateUtils.ts
@@ -0,0 +1,66 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import fr from 'dayjs/locale/fr'
+
+export type DateFormatType = 'input' | 'shortText' | 'longText' | 'default' | 'full-input'
+
+type SingleDateProp = Date | string
+type NullableSingleDateProp = SingleDateProp | null | undefined
+type DateProp = NullableSingleDateProp | NullableSingleDateProp[]
+
+type DateUtilsConfig = {
+  utc?: boolean
+  separator?: string
+  unique?: boolean
+} & ({ format?: DateFormatType } | { format?: string })
+
+const formats: Record<DateFormatType, string> = {
+  'shortText': 'ddd DD/MM',
+  'longText': 'ddd DD MMM YYYY',
+  'input': 'YYYY-MM-DD',
+  'full-input': 'YYYY-MM-DD H:mm:ss',
+  'default': 'DD/MM/YYYY',
+}
+
+export function dateUtils() {
+  dayjs.extend(utc)
+  dayjs.locale(fr)
+
+  function formatDate(date: DateProp, config: DateUtilsConfig = {}): string {
+    if (Array.isArray(date)) {
+      const formattedDates = date.filter(d => !!d).map(d => formatDate(d, config))
+      if (config.unique)
+        return [...(new Set(formattedDates))].join(config.separator ?? ', ')
+      return formattedDates.join(config.separator ?? ', ')
+    }
+
+    if (!date)
+      return ''
+
+    const format = config.format || 'default'
+
+    return (config.utc ? dayjs.utc(date) : dayjs(date)).format(Object.prototype.hasOwnProperty.call(formats, format) ? formats[format as keyof typeof formats] : format)
+  }
+
+  function isBetween(date: SingleDateProp, start: NullableSingleDateProp, end: NullableSingleDateProp, utc = false) {
+    const d = utc ? dayjs.utc(date) : dayjs(date)
+    return (start ? d.isAfter(start) : true) && (end ? d.isBefore(end) : true)
+  }
+
+  return {
+    defaultFormat: (date: DateProp, config: DateUtilsConfig = {}) => {
+      return formatDate(date, { ...config, format: 'default' })
+    },
+    shortTextFormat: (date: DateProp, config: DateUtilsConfig = {}) => {
+      return formatDate(date, { ...config, format: 'shortText' })
+    },
+    inputFormat: (date: DateProp, config: DateUtilsConfig = {}) => {
+      return formatDate(date, { ...config, format: 'input' })
+    },
+    inputFullFormat: (date: DateProp, config: DateUtilsConfig = {}) => {
+      return formatDate(date, { ...config, format: 'full-input' })
+    },
+    formatDate,
+    isBetween,
+  }
+}
