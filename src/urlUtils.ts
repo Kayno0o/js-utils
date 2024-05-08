a0d26bc5feb58f0ad26f8ecc24ef160e836cd2be
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-05-08 22:11:01 +0200

    feat: filter undefined and null for url query builder

--- src/urlUtils.ts
+++ src/urlUtils.ts
@@ -1,3 +1,3 @@
 export function buildQuery(query?: object) {
-  return query ? `?${Object.entries(query).filter(e => !!e[1]).map(e => `${e[0]}=${e[1]}`).join('&')}` : ''
+  return query ? `?${Object.entries(query).filter(e => e[1] !== undefined && e[1] !== null).map(e => `${e[0]}=${e[1]}`).join('&')}` : ''
 }
