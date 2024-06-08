df2a3a883388dd8b46c50d51f837492a61a62cbc
Kayno0o <kevyn.fyleyssant@gmail.com>
2024-06-08 13:36:08 +0200

    feat: add array range and array not empty

--- src/arrayUtils.ts
+++ src/arrayUtils.ts
@@ -8,3 +8,11 @@ export function getRandomElement<T>(values: T[], isCrypto = false): T {
 export function uniqueArray<T extends string | number | symbol | boolean | null | undefined>(arr: T[]): T[] {
   return [...new Set(arr)]
 }
+
+export function range(nb: number) {
+  return [...Array(nb).keys()]
+}
+
+export function notEmpty<T>(value: T | null | undefined): value is T {
+  return value !== null && value !== undefined
+}
